aaa70ca5cb529831f0b20ec08eb43d88a98c68cd
Adam Tutko <atutko@caladan.eecs.utk.edu>
2021-04-03 11:06:12 -0400

    Added ebpfvisual script

---
+++ ebpfvisual.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import psutil
+import sys
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+
+if len(sys.argv) < 2:
+	print ("Expected input: ./ebpfvisual Flags (-b for base) Process (optional)")
+
+
+
