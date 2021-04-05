784697913984bc6a6154aa25be675fc77fde17a1
Dylan Lee <dlee97@vols.utk.edu>
2021-04-05 14:26:39 -0400

    added parameter to adjust run time

--- ebpfv.sh
+++ ebpfv.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+iterations=$1
+
 echo "Starting benchmark program to run for 100 seconds."
-./a.out & > /dev/null
+./a.out > /dev/null &
 
-echo "Running cachestat for 100 seconds."
-./tools/cachestat.py 1 100 > output/cache_stdout &
+echo "Running cachestat for $1 seconds."
+./tools/cachestat.py 1 $iterations > output/cache_stdout &
 
-echo "Running llcstat for 100 seconds."
-./tools/llcstat.py 100 > output/llc_stdout &
+echo "Running llcstat for $1 seconds."
+./tools/llcstat.py $iterations > output/llc_stdout &
 
-echo "Running biotop for 5 seconds, 10 times."
-./tools/biotop.py 5 5 | grep "a.out" > output/biotop_stdout & 
+echo "Running biotop for $1 seconds, 1 times."
+./tools/biotop.py $iterations 1 | grep "a.out" > output/biotop_stdout & 
 
 
 echo "...running..."
