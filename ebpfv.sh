b3b0870b6ea4d9fe22cf2c08abcd7a6869ed0587
Dylan Lee <dlee97@vols.utk.edu>
2021-04-05 14:18:10 -0400

    adding more tools

--- ebpfv.sh
+++ ebpfv.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+echo "Starting benchmark program to run for 100 seconds."
+./a.out & > /dev/null
+
 echo "Running cachestat for 100 seconds."
-./tools/cachestat.py 1 100 > ouput/cache_stdout &
+./tools/cachestat.py 1 100 > output/cache_stdout &
 
 echo "Running llcstat for 100 seconds."
 ./tools/llcstat.py 100 > output/llc_stdout &
 
+echo "Running biotop for 5 seconds, 10 times."
+./tools/biotop.py 5 5 | grep "a.out" > output/biotop_stdout & 
 
+
+echo "...running..."
+
+wait
+
+echo "Finished."
 
 # usage getpid [varname] 
 # function to get the pid of a process
